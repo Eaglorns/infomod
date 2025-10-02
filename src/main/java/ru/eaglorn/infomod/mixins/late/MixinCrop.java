b16f0a58d6141f6c668eaf9a08c2f854fb267eab
Eaglorns <eaglorn@yandex.ru>
2025-10-03 09:50:29 +1000

    update

--- src/main/java/ru/eaglorn/infomod/mixins/late/MixinCrop.java
+++ src/main/java/ru/eaglorn/infomod/mixins/late/MixinCrop.java
@@ -2,30 +2,36 @@ package ru.eaglorn.infomod.mixins.late;
 
 import ic2.core.crop.TileEntityCrop;
 
-import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TileEntityCrop.class)
 public class MixinCrop {
+    @Redirect(
+        method = "tick",
+        at = @At(
+            value = "INVOKE",
+            target = "Lic2/core/crop/TileEntityCrop;generateWeed()V"
+        ),
+        remap = false
+    )
+    private void redirectGenerateWeed(TileEntityCrop instance) {
+    }
+
     @Inject(
         method = "tick",
         at = @At(
-            value = "JUMP",
-            opcode = Opcodes.IFNULL,
+            value = "INVOKE",
+            target = "Ljava/util/Random;nextInt(I)I",
             ordinal = 0
         ),
         remap = false,
         cancellable = true
     )
-    private void skipCropNullCheck(CallbackInfo ci) {
-        ci.cancel();
-    }
-
-    @Inject(method = {"generateWeed"}, at = {@At("HEAD")}, remap = false, cancellable = true)
-    public void returnBeforeGenerate(CallbackInfo ci) {
+    private void removeWeedCreation(CallbackInfo ci) {
         ci.cancel();
     }
 }
